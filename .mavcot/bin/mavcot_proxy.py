9d36b904e01451a13fe5fca8db65743b1d09d97c
sn0wl3r0ker <lin900123@gmail.com>
2026-03-26 21:16:27 +0800

    update

---
+++ .mavcot/bin/mavcot_proxy.py
@@ -0,0 +1,4 @@
+#!/home/syslabros/Desktop/mavcot/.mavcot/bin/python3
+# EASY-INSTALL-SCRIPT: 'MAVCOT==0.1.dev0','mavcot_proxy.py'
+__requires__ = 'MAVCOT==0.1.dev0'
+__import__('pkg_resources').run_script('MAVCOT==0.1.dev0', 'mavcot_proxy.py')
